761b98f00feb369289a685f0d06606642bfec457
Nicolás Biojo Bermeo <nicolas.biojo@correo.icesi.edu.co>
2019-07-28 17:30:14 -0500

    Update gui

--- phoneCompany/src/gui/AddPanel.java
+++ phoneCompany/src/gui/AddPanel.java
@@ -41,30 +41,30 @@ public class AddPanel extends JFrame implements ActionListener {
 		this.type = type;
 		this.main = main;
 		
-		setTitle("Add");
+		setTitle("Add "+type);
 		setSize(400, 400);
 		setLocationRelativeTo(null);
 		setLayout(new BorderLayout());
 		
-		lbCedula = new JLabel("Cedula", SwingConstants.CENTER);
+		lbCedula = new JLabel("Cedula: ", SwingConstants.RIGHT);
 		lbCedula.setFont(new java.awt.Font("Calibri", 1, 28));
 		
-		lbContrasenha = new JLabel("Contrasenia", SwingConstants.CENTER);
+		lbContrasenha = new JLabel("Contrasenia: ", SwingConstants.RIGHT);
 		lbContrasenha.setFont(new java.awt.Font("Calibri", 1, 28));
 		
-		lbDireccion = new JLabel("Direccion", SwingConstants.CENTER);
+		lbDireccion = new JLabel("Direccion: ", SwingConstants.RIGHT);
 		lbDireccion.setFont(new java.awt.Font("Calibri", 1, 28));
 		
-		lbFecha = new JLabel("Fecha", SwingConstants.CENTER);
+		lbFecha = new JLabel("Fecha: ", SwingConstants.RIGHT);
 		lbFecha.setFont(new java.awt.Font("Calibri", 1, 28));
 		
-		lbId = new JLabel("Id", SwingConstants.CENTER);
+		lbId = new JLabel("Id: ", SwingConstants.RIGHT);
 		lbId.setFont(new java.awt.Font("Calibri", 1, 28));
 		
-		lbNombre = new JLabel("Nombre", SwingConstants.CENTER);
+		lbNombre = new JLabel("Nombre: ", SwingConstants.RIGHT);
 		lbNombre.setFont(new java.awt.Font("Calibri", 1, 28));
 		
-		lbTelefono = new JLabel("Telefono", SwingConstants.CENTER);
+		lbTelefono = new JLabel("Telefono: ", SwingConstants.RIGHT);
 		lbTelefono.setFont(new java.awt.Font("Calibri", 1, 28));
 		
 		txtCedula = new TextField("");
@@ -75,37 +75,82 @@ public class AddPanel extends JFrame implements ActionListener {
 		txtNombre = new TextField("");
 		txtTelefono = new TextField("");
 		
+		butAdd = new JButton("Find");
+		butAdd.setFont(new java.awt.Font("Calibri", 1, 18));
+		butAdd.setActionCommand(ADD);
+		butAdd.addActionListener(this);
+		
 		JPanel data = new JPanel();
-		data.setLayout(new GridLayout(7, 2));
+		data.setLayout(new GridLayout(15, 2));
+		
+		data.add(new JLabel());
+		data.add(new JLabel());
 		
 		data.add(lbId);
 		data.add(txtId);
 		
+		data.add(new JLabel());
+		data.add(new JLabel());
+		
 		data.add(lbContrasenha);
 		data.add(txtContrasenha);
+		
+		data.add(new JLabel());
+		data.add(new JLabel());
 		
 		data.add(lbNombre);
 		data.add(txtNombre);
 		
+		data.add(new JLabel());
+		data.add(new JLabel());
+		
 		data.add(lbCedula);
 		data.add(txtCedula);
+		
+		data.add(new JLabel());
+		data.add(new JLabel());
 		
 		data.add(lbDireccion);
 		data.add(txtDireccion);
 		
+		data.add(new JLabel());
+		data.add(new JLabel());
+		
 		data.add(lbFecha);
 		data.add(txtFecha);
+		
+		data.add(new JLabel());
+		data.add(new JLabel());
 		
 		data.add(lbTelefono);
 		data.add(txtTelefono);
 		
+		data.add(new JLabel());
+		data.add(new JLabel());
+		
+		JPanel button = new JPanel();
+		button.setLayout(new GridLayout(2,3));
+		
+		button.add(new JLabel());
+		button.add(butAdd);
+		button.add(new JLabel());
+		
+		button.add(new JLabel());
+		button.add(new JLabel());
+		button.add(new JLabel());
+		
 		add(data, BorderLayout.CENTER);
+		add(button, BorderLayout.SOUTH);
+		add(new JLabel("          "),BorderLayout.EAST);
 		
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+
+		if (command.equals(ADD)) {	
+			
+		} 
 	}
 }
--- phoneCompany/src/gui/FindPanel.java
+++ phoneCompany/src/gui/FindPanel.java
@@ -14,9 +14,8 @@ import javax.swing.SwingConstants;
 
 public class FindPanel extends JFrame implements ActionListener  {
 
-	public static final String ADD = "Add";
-	
-	private JButton butAdd;
+	public static final String FIND = "FIND";
+	private JButton butFind;
 	
 	private JLabel lbId;
 	private JLabel lbContrasenha;
@@ -41,7 +40,7 @@ public class FindPanel extends JFrame implements ActionListener  {
 		this.type = type;
 		this.main = main;
 		
-		setTitle("Find");
+		setTitle("Find "+type);
 		setSize(400, 400);
 		setLocationRelativeTo(null);
 		setLayout(new BorderLayout());
@@ -75,36 +74,82 @@ public class FindPanel extends JFrame implements ActionListener  {
 		txtNombre = new TextField("");
 		txtTelefono = new TextField("");
 		
+		butFind = new JButton("Find");
+		butFind.setFont(new java.awt.Font("Calibri", 1, 18));
+		butFind.setActionCommand(FIND);
+		butFind.addActionListener(this);
+		
 		JPanel data = new JPanel();
-		data.setLayout(new GridLayout(7, 2));
+		data.setLayout(new GridLayout(15, 2));
+		
+		data.add(new JLabel());
+		data.add(new JLabel());
 		
 		data.add(lbId);
 		data.add(txtId);
 		
+		data.add(new JLabel());
+		data.add(new JLabel());
+		
 		data.add(lbContrasenha);
 		data.add(txtContrasenha);
+		
+		data.add(new JLabel());
+		data.add(new JLabel());
 		
 		data.add(lbNombre);
 		data.add(txtNombre);
 		
+		data.add(new JLabel());
+		data.add(new JLabel());
+		
 		data.add(lbCedula);
 		data.add(txtCedula);
+		
+		data.add(new JLabel());
+		data.add(new JLabel());
 		
 		data.add(lbDireccion);
 		data.add(txtDireccion);
 		
+		data.add(new JLabel());
+		data.add(new JLabel());
+		
 		data.add(lbFecha);
 		data.add(txtFecha);
+		
+		data.add(new JLabel());
+		data.add(new JLabel());
 		
 		data.add(lbTelefono);
 		data.add(txtTelefono);
 		
+		data.add(new JLabel());
+		data.add(new JLabel());
+		
+		JPanel button = new JPanel();
+		button.setLayout(new GridLayout(2,3));
+		
+		button.add(new JLabel());
+		button.add(butFind);
+		button.add(new JLabel());
+		
+		button.add(new JLabel());
+		button.add(new JLabel());
+		button.add(new JLabel());
+		
 		add(data, BorderLayout.CENTER);
+		add(button, BorderLayout.SOUTH);
+		add(new JLabel("          "),BorderLayout.EAST);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		String command = e.getActionCommand();
+
+		if (command.equals(FIND)) {	
+			
+		}
 		
 	}
 }
